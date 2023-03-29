7371f3af4db7d24b74cbef2f48eafbb7d3531330
Edward Didyk <eduard.didyk01@gmail.com>
2023-03-29 20:21:32 +0300

    cart reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -36,7 +37,9 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
   // const { currentUser } = state
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+    dispatch(
+      createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
+    );
   }
   const value = { currentUser, setCurrentUser };
 
