2df05067cd4c6b419b7ae679b8f124376f8e42bd
Edward Didyk <eduard.didyk01@gmail.com>
2023-04-10 03:16:47 +0300

    category saga

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -3,9 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview";
 import Category from "../category/category";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase";
+// import { getCategoriesAndDocuments } from "../../utils/firebase/firebase";
 // import { setCategories } from "../../store/categories/category.action";
-import { fetchCategoriesAsync } from "../../store/categories/category.action";
+// import { fetchCategoriesAsync } from "../../store/categories/category.action";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 
 export default function Shop() {
   const dispatch = useDispatch();
@@ -17,7 +18,7 @@ export default function Shop() {
     //   dispatch(setCategories(categoriesArray));
     // }
     // getCategoriesMap();
-    dispatch(fetchCategoriesAsync());
+    dispatch(fetchCategoriesStart());
   }, []);
 
   return (
