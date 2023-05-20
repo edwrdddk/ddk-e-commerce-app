be56d0e7f3c91c87c62e85f396d6dfaa41f98ddf
Edward Didyk <eduard.didyk01@gmail.com>
2023-05-20 03:06:02 +0300

    Full TS conversion

--- src/routes/authentication/authentication.tsx
+++ src/routes/authentication/authentication.tsx
@@ -1,5 +1,5 @@
-import SingUpForm from "../../components/sing-up-form/sing-up-form.tsx";
-import SingInForm from "../../components/sing-in-form/sing-in-form.tsx";
+import SingUpForm from "../../components/sing-up-form/sing-up-form";
+import SingInForm from "../../components/sing-in-form/sing-in-form";
 import "./authentication.scss";
 
 export default function Authentication() {
