2145ee5a2528f537be222a64bb8726d27a363a1d
Edward Didyk <eduard.didyk01@gmail.com>
2023-05-26 00:24:46 +0300

    Cart dropdown component performance optimization via useCallback

--- src/components/cart-dropdown/cart-dropdown.tsx
+++ src/components/cart-dropdown/cart-dropdown.tsx
@@ -1,4 +1,5 @@
 // import { useContext } from "react";
+import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -14,9 +15,9 @@ export default function CartDropdown() {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const goToCheckOutHandler = () => {
+  const goToCheckOutHandler = useCallback(() => {
     navigate("/checkout");
-  }
+  }, []);
 
   return (
     <CartDropdownContainer>
@@ -33,3 +34,42 @@ export default function CartDropdown() {
     </CartDropdownContainer>
   )
 }
+
+
+// pre performance optimization
+
+// import { useContext } from "react";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+
+// // import { CartContext } from "../../contexts/cart.context";
+
+// import Button from "../button/button";
+// import CartItem from "../cart-item/cart-item";
+// import { selectCartItems } from "../../store/cart/cart.selector";
+// import {CartDropdownContainer, EmptyMessage, CartItems} from "./cart-dropdown.styles";
+ 
+// export default function CartDropdown() {
+//   // const { cartItems } = useContext(CartContext);
+//   const cartItems = useSelector(selectCartItems);
+//   const navigate = useNavigate();
+
+//   const goToCheckOutHandler = () => {
+//     navigate("/checkout");
+//   };
+
+//   return (
+//     <CartDropdownContainer>
+//       <CartItems>
+//       {
+//         cartItems.length ? (cartItems.map((item) => (
+//         <CartItem key={item.id} cartItem={item} />
+//       ))) : (
+//         <EmptyMessage>Your cart is empty</EmptyMessage>
+//       )
+//       }
+//       </CartItems>
+//       <Button onClick={goToCheckOutHandler}>GO TO CHECKOUT</Button>
+//     </CartDropdownContainer>
+//   )
+// }
