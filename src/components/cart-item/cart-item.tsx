f09d44546ccbc9af8a993b07ccfd9702feb46d31
Edward Didyk <eduard.didyk01@gmail.com>
2023-05-26 01:28:42 +0300

    React memo

--- src/components/cart-item/cart-item.tsx
+++ src/components/cart-item/cart-item.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 import { CartItemContainer, ItemDetails } from './cart-item.styles';
 import { CartItem as TCartItem } from '../../store/cart/cart.types';
 
@@ -6,7 +6,7 @@ type CartItemProps = {
   cartItem: TCartItem;
 }
 
-const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+const CartItem: FC<CartItemProps> = memo(({ cartItem }) => {
   const { name, quantity, price, imageUrl } = cartItem;
   return (
     <CartItemContainer>
@@ -17,6 +17,6 @@ const CartItem: FC<CartItemProps> = ({ cartItem }) => {
       </ItemDetails>
     </CartItemContainer>
   )
-}
+})
 
 export default CartItem;
